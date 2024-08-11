af243a4b678ec697a338cf605d74c7ede0bd4f17
Pedro Rogério <138505724+Rogerp314@users.noreply.github.com>
2024-08-11 20:55:24 -0300

    Ajustes

--- gerador_nomes.py
+++ gerador_nomes.py
@@ -1,7 +1,7 @@
 from random import *
 from tkinter import *
 #Função que vai gerar o nome
-def nome():
+def nome(event):
     escolha = Label(tela, text='''Qual sua classe?\n 
                        [ 1 ] Mago\n 
                        [ 2 ] Ladino\n 
@@ -13,13 +13,24 @@ def nome():
                        [ 8 ] Bruxo\n
                     Escolha: ''')
     escolha.pack()
-    opção = Entry(tela).pack()
-    classe = [ 'Mago', 'Ladino', 'Necromante', 'Cavaleiro', 'Arqueiro', 'Bardo', 'Feitiçeiro', 'Bruxo']
-    letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    caracteres = (input('Quantos caractéres seu nome terá? '))
-    for s in range(0, caracteres):
-        sorteio = choice(letras)
-        print('{} o {}'.format(sorteio.capitalize(), classe[0]), end='')
+    opção = Entry(tela)
+    opção.pack()
+    botao = Button(tela, text='Clique aqui para confirmar o classe', command = sorvete)
+    botao.pack()
+    valor_classe = opção.get()
+    valor_real_classe = int(valor_classe)
+    def sorvete():
+        classe = [ 'Mago', 'Ladino', 'Necromante', 'Cavaleiro', 'Arqueiro', 'Bardo', 'Feitiçeiro', 'Bruxo']
+        letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        caracteres_pergunta = Label(tela, text='Quantos caractéres seu nome terá? ')
+        caracteres_pergunta.pack()
+        caracteres = Entry(tela)
+        caracteres.pack()
+        valor_caracteres = caracteres.get()
+        valor_real_caracteres = int(valor_caracteres)
+        for s in range(0, valor_real_caracteres):
+            sorteio = choice(letras)
+            Label(tela, text='{} o {}'.format(sorteio.capitalize(), classe[valor_real_classe]), end='')
     
 #Fazer com que o print da classe fique na mesma linha que a do nome e por fim colocar o programa na janela e personalizala. =)
 
@@ -28,8 +39,19 @@ def nome():
 tela = Tk()
 tela.title('Gerador de nomes RPG')
 tela.geometry('800x600')
-#Criando Botão
-botao = Button(tela, text='Teste', command=nome)
-botao.pack()
+#Fazendo com que ao clicado a tecla Enter começe o algoritmo
+verificador = False
+
+start = Label(tela, text='Clique Enter para começar!')
+start.pack()
+tela.bind('<Return>', nome)
+verificador = True
+def iniciar_codigo(event):
+    global verificador
+    if not verificador:
+        print("Código iniciado!")
+        verificador = True
+    else:
+        print("Ação já foi executada.")
 
 tela.mainloop()
