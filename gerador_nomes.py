c0a6bb54a98b46f757f5535f54f14d7e6a54aaac
Pedro Rogério <138505724+Rogerp314@users.noreply.github.com>
2024-08-23 00:46:44 -0300

    Atualização

--- gerador_nomes.py
+++ gerador_nomes.py
@@ -1,8 +1,11 @@
-from random import *
-from tkinter import *
-#Função que vai gerar o nome
-def nome(event):
-    escolha = Label(tela, text='''Qual sua classe?\n 
+from random import randint
+
+print('\033[31m-=\033[m'*75)
+print('Esse é um programa de geração de nomes de personagens de RPG')
+opção = input('Quer começar?[S/N]\nEscolha: ').strip().upper()
+letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+if opção == 'S':
+    classe = int(input('''Qual sua classe?\n 
                        [ 1 ] Mago\n 
                        [ 2 ] Ladino\n 
                        [ 3 ] Necromante\n 
@@ -11,47 +14,22 @@ def nome(event):
                        [ 6 ] Bardo\n 
                        [ 7 ] Feitiçeiro\n
                        [ 8 ] Bruxo\n
-                    Escolha: ''')
-    escolha.pack()
-    opção = Entry(tela)
-    opção.pack()
-    botao = Button(tela, text='Clique aqui para confirmar o classe', command = sorvete)
-    botao.pack()
-    valor_classe = opção.get()
-    valor_real_classe = int(valor_classe)
-    def sorvete():
-        classe = [ 'Mago', 'Ladino', 'Necromante', 'Cavaleiro', 'Arqueiro', 'Bardo', 'Feitiçeiro', 'Bruxo']
-        letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-        caracteres_pergunta = Label(tela, text='Quantos caractéres seu nome terá? ')
-        caracteres_pergunta.pack()
-        caracteres = Entry(tela)
-        caracteres.pack()
-        valor_caracteres = caracteres.get()
-        valor_real_caracteres = int(valor_caracteres)
-        for s in range(0, valor_real_caracteres):
-            sorteio = choice(letras)
-            Label(tela, text='{} o {}'.format(sorteio.capitalize(), classe[valor_real_classe]), end='')
-    
-#Fazer com que o print da classe fique na mesma linha que a do nome e por fim colocar o programa na janela e personalizala. =)
-
-
-#Criando tela
-tela = Tk()
-tela.title('Gerador de nomes RPG')
-tela.geometry('800x600')
-#Fazendo com que ao clicado a tecla Enter começe o algoritmo
-verificador = False
-
-start = Label(tela, text='Clique Enter para começar!')
-start.pack()
-tela.bind('<Return>', nome)
-verificador = True
-def iniciar_codigo(event):
-    global verificador
-    if not verificador:
-        print("Código iniciado!")
-        verificador = True
-    else:
-        print("Ação já foi executada.")
-
-tela.mainloop()
+                    Escolha: '''))
+    clas = [ 'Mago', 'Ladino', 'Necromante', 'Cavaleiro', 'Arqueiro', 'Bardo', 'Feitiçeiro', 'Bruxo']
+    letras_nome = []
+    quantidade_letras = int(input('Quantos caractéres você vai querer em seu nome? '))
+    for x in range(1, quantidade_letras+1):
+        nome = randint(0, quantidade_letras)
+        nome_sorteado = letras[nome]
+        letras_nome.append(nome_sorteado)
+    classe_completa = clas[classe-1]
+    nome_completo = ''.join(letras_nome)
+    print('Seu nome é:')
+    print('{}, {}'.format(classe_completa, nome_completo.title()))
+elif opção == 'N':
+    print('FIM DO PROGRAMA')
+else:
+    while opção != 'S':
+        print('\033[31m[ERROR]\033[m')
+        opção = input('Quer começar o programa?[S/N]\nEscolha: ').strip().upper()
+print('\033[31m-=\033[m'*75)
--- teste.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-if hasattr(sys, 'real_prefix') or hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix:
-    print('Ambiente virtual')
-else:
-    print('Não é virtual')
